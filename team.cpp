475323df7bd4ab819dbe30d86aeee60d3db0908c
GrispenX <grispenx@gmail.com>
2023-12-17 00:31:47 +0200

    Class Team declared, its methods realized
    build.sh created

---
+++ team.cpp
@@ -0,0 +1,49 @@
+#include "team.h"
+
+Team::Team(std::string name)
+{
+    m_name = name;
+}
+
+bool Team::addPlayer(Player player, Hero hero)
+{
+    for(int i = 0; i < TEAM_SIZE; i++)
+    {
+        if(team[i].has_value())
+        {
+            if(team[i].value().first.getId() == player.getId())
+            {
+                return false;
+            }
+        }
+    }
+    for(int i = 0; i < TEAM_SIZE; i++)
+    {
+        if(!team[i].has_value())
+        {
+            team[i] = std::make_pair(player, hero);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Team::removePlayer(uint16_t id)
+{
+    for(int i = 0; i < TEAM_SIZE; i++)
+    {
+        if(team[i].has_value())
+        {
+            if(team[i].value().first.getId() == id)
+            {
+                team[i].reset();
+                return true;
+            }
+        }
+    }
+    return false;
+}
+std::optional<std::pair<Player, Hero>> Team::getPlayerByIndex(int index)
+{
+    return team[index];
+}
---
+++ team.h
@@ -0,0 +1,19 @@
+#include "playerManager.h"
+#include "heroManager.h"
+#include <optional>
+
+#define TEAM_SIZE 5
+
+class Team
+{
+private:
+    std::string m_name;
+    std::optional<std::pair<Player, Hero>> team[TEAM_SIZE];
+public:
+    Team() = delete;
+    Team(std::string name);
+
+    bool addPlayer(Player player, Hero hero);
+    bool removePlayer(uint16_t id);
+    std::optional<std::pair<Player, Hero>> getPlayerByIndex(int index);
+};
