d26af3a51ab8d77d95d195dd61d34f35347034bf
GrispenX <grispenx@gmail.com>
2023-12-16 15:11:44 +0200

    Declared and realized class HeroManager

---
+++ heroManager.h
@@ -0,0 +1,17 @@
+#include "hero.h"
+#include <vector>
+
+class HeroManager
+{
+private:
+    std::vector<Hero> heroList;
+    uint16_t next_id = 0;
+    uint16_t genId();
+public:
+    Hero* CreateHero(std::string name, uint16_t hp, uint16_t damage);
+    bool DeleteHero(uint16_t id);
+
+    Hero* getHeroById(uint16_t id);
+    Hero* getHeroByName(std::string name);
+    void showHeroInfo(Hero* hero);
+};
