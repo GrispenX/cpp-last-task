3c0b1ee8b1dbfa756a819bc20be90ad1784716f6
GrispenX <grispenx@gmail.com>
2023-12-16 02:09:40 +0200

    Realized Player methods

---
+++ player.cpp
@@ -0,0 +1,15 @@
+#include <player.h>
+#include <string>
+
+#define START_RANK 250
+
+Player::Player(uint16_t id, std::string name)
+{
+    m_id = id;
+    m_name = name;
+    m_rank = START_RANK;
+}
+
+uint16_t Player::getId() {return m_id;}
+std::string Player::getName() {return m_name;}
+uint16_t Player::getRank() {return m_rank;}
