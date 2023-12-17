7dc97118c277b46e0baf4300159d567e91f8bc47
GrispenX <grispenx@gmail.com>
2023-12-17 12:24:24 +0200

    Added Player and Hero lists

---
+++ PlayerHeroList.cpp
@@ -0,0 +1,2 @@
+#include "PlayerHeroList.h"
+
---
+++ PlayerHeroList.h
@@ -0,0 +1,60 @@
+#include "playerManager.h"
+#include "heroManager.h"
+
+std::string pnamelist[20] =
+{
+    "GrispenX",
+    "Yura",
+    "Valera",
+    "Sanya",
+    "LayLean",
+    "Solyarka",
+    "Sestra Sani",
+    "Anzhela",
+    "Prus",
+    "aharook",
+    "Sheptitskiy",
+    "haxdp",
+    "forg",
+    "hismajestyniko",
+    "somiknet",
+    "saucekari",
+    "Pornotraktor",
+    "davatape",
+    "David",
+    "noname"
+};
+
+std::string hmanelist[20] =
+{
+    "Anti-Mage",
+    "Axe",
+    "Puck",
+    "Storm Spirit",
+    "Faceless Void",
+    "Tiny",
+    "Morphling",
+    "Invoker",
+    "Nature's Prophet",
+    "Legion Commander",
+    "Monkey Kind",
+    "Mars",
+    "Dawnbreacker",
+    "Hoodwink",
+    "Ember Spirit",
+    "Slark",
+    "Meepo",
+    "Templar Assassin",
+    "Luna",
+    "Medusa"
+};
+
+uint16_t hhplist[20] =
+{
+    73, 91, 194, 313, 314, 319, 420, 436, 539, 551, 590, 812, 826, 894, 1039, 1200, 1260, 1295, 1544, 1619
+};
+
+uint16_t hdamagelist[20] =
+{
+    1480, 1408, 1324, 1270, 1250, 1066, 1034, 869, 800, 737, 730, 612, 589, 584, 579, 515, 482, 470, 306, 172
+};
